93da3657d250ad1c876c3204ff85d7a99e2bb01d
yezinnie14 <yezinnie14@gmail.com>
2026-01-06 15:13:57 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 import App from './App';
 import './assets/scss/global.scss';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
 
