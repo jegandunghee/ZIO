219c2c0120084971649e613f47463307c3b442b4
yezinnie14 <yezinnie14@gmail.com>
2026-01-06 15:02:30 +0900

    update

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,8 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
-import Footer from "./Footer";
-import GnbDesktop from "./GnbDesktop";
-import GnbMobile from "./GnbMobile";
+import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
+import GnbDesktop from "../components/common/GnbDesktop";
+import GnbMobile from "../components/common/GnbMobile";
 const Layout = () => {
   return (
     <div id="app">
