d1dd32077a00ab00afd820049fd9f9999f469614
jegandunghee <jaejea1217@gmail.com>
2026-01-08 11:50:12 +0900

    api 구현 완료

--- src/App.js
+++ src/App.js
@@ -6,26 +6,32 @@ import ReservationPage from "./pages/ReservationPage";
 import PaymentPage from "./pages/PaymentPage";
 import MyPage from "./pages/MyPage";
 import Tip from "./pages/Tip";
+import AuthPage from "./pages/AuthPage"
+
+import { AuthProvider } from "./contexts/AuthContext";
+import { ParkingProvider } from "./contexts/ParkingContext";
+import { ReservationProvider } from "./contexts/ReservationContext";
 
 import "./App.scss";
 
 const App = () => {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<MainPage />} />
-
-        <Route path="/parking/:parkingId" element={<ReservationPage />} />
-
-
-        <Route path="/payment" element={<PaymentPage />} />
-
-
-        <Route path="/mypage" element={<MyPage />} />
-
-        <Route path="/tip" element={<Tip />} />
-      </Route>
-    </Routes>
+    <AuthProvider>
+      <ParkingProvider>
+        <ReservationProvider>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/parking/:parkingId" element={<ReservationPage />} />
+              <Route path="/payment" element={<PaymentPage />} />
+              <Route path="/mypage" element={<MyPage />} />
+              <Route path="/tip" element={<Tip />} />
+              <Route path="/auth" element={<AuthPage />} />
+            </Route>
+          </Routes>
+        </ReservationProvider>
+      </ParkingProvider>
+    </AuthProvider>
   );
 };
 
