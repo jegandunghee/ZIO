0e4248d2bbeef8e63a725953e67c6c3cd04d18ca
hyumm1380-maker <hyunn1380@gmail.com>
2026-01-08 18:07:59 +0900

    오류가 났지만 우선 저장

--- src/pages/section/payment/Clock.js
+++ src/pages/section/payment/Clock.js
@@ -1,39 +1,50 @@
-import { useState } from 'react'
-import DatePicker from 'react-datepicker';
 import "./Clock.scss";
+import Select from 'react-select'
+import React, { useState } from 'react';
+
+const Checkbox = ({ children, ...props }: JSX.IntrinsicElements['input']) => (
+  <label style={{ marginRight: '1em' }}>
+    <input type="checkbox" {...props} />
+    {children}
+  </label>
+);
 
 const Clock = () => {
-  const [startTime, setStartTime] = useState(
-    new Date()
-  );
-  const [endTime, setEndTime] = useState(
-    new Date()
-  );
+  const [isClearable, setIsClearable] = useState(true);
+
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+    { value: 'vanilla2', label: 'Vanilla2' },
+    { value: 'vanilla3', label: 'Vanilla3' },
+    { value: 'vanilla4', label: 'Vanilla4' }
+  ];
+
+    <Select options={options} />
 
   return (
     <section id='clock'>
       <div className='in-time'>
         <p>입차 시간</p>
-        <DatePicker
-          selected={startTime}
-          onChange={setStartTime}
-          showTimeSelect
-          showTimeSelectOnly
-          timeIntervals={60}
-          timeCaption="입차시간"
-          dateFormat="hh:mm aa"
+        <Select
+          className="basic-single"
+          classNamePrefix="select"
+          // defaultValue={options[0]}
+          isClearable={isClearable}
+          name="color"
+          options={options}
         />
       </div>
       <div className='out-time'>
         <p>출차 시간</p>
-        <DatePicker 
-          selected={endTime}
-          onChange={setEndTime}
-          showTimeSelect
-          showTimeSelectOnly
-          timeIntervals={60}
-          timeCaption="출차시간"
-          dateFormat="hh:mm aa"
+        <Select
+          className="basic-single"
+          classNamePrefix="select"
+          // defaultValue={colourOptions[0]}
+          isClearable={isClearable}
+          name="color"
+          options={options}
         />
       </div>
     </section>
